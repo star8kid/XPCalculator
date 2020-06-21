8bbb7ba17124f75f0ba778ea71962f2b51ab9e10
star8kid <2022younganthonyn@aaps.k12.mi.us>
2020-06-20 20:12:22 -0400

    Create either classes for each new window or make whole app under a single class

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,17 +1,59 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import font
+import time
 
 
 class StartApp():
     def __init__(self):
+
+        # 1. All the window and button definitions
         self.root = Tk()
 
-        self.menu_window = ttk.Frame(self.root, padding = "10 10 10 10")
 
+
+        
+        self.menu_window = ttk.Frame(self.root, padding = "100 10 100 10")
+        self.title_font = font.Font( family = "System", size = 18 , weight = "bold")
+        self.title_label = ttk.Label(self.menu_window, text = "XP Calculator", font = self.title_font, padding = "5 20 5 50")
+        self.title_label.grid(row = 0 , column = 1)
+
+        self.padVerticalValue = 3
         self.menuButton1 = ttk.Button(self.menu_window)
-        self.menuButton1.grid( row = 1 ,column = 1)
+        self.menuButton1.grid( row = 1 , column = 1 , pady = self.padVerticalValue)
+        self.menuButton2 = ttk.Button(self.menu_window)
+        self.menuButton2.grid( row = 2 , column = 1 , pady = self.padVerticalValue)
+        self.menuButton3 = ttk.Button(self.menu_window)
+        self.menuButton3.grid( row = 3 , column = 1 , pady = self.padVerticalValue)
+        self.menuQuit = ttk.Button(self.menu_window)
+        self.menuQuit.grid( row = 4, column = 1 , pady = self.padVerticalValue)
+
+
+        self.from_zero_window = Toplevel()
+        self.from_zero_window.title("Calculate From Level Zero")
+        
+
+
+
+
+        # 2. The button configurations
+
+        self.menuButton1.config( text = "Calculate From Level Zero", command = lambda : self.openCalcZero())
+
+        self.quitStyle = ttk.Style()
+        self.quitStyle.configure("Quit.TButton", foreground = "dark red")
+        self.menuQuit.config( text = "Quit" , style = "Quit.TButton" , command = lambda : self.root.quit())
+
+        self.from_zero_window.withdraw()
 
         self.menu_window.pack()
+    # 3. The method configurations
+
+    def openCalcZero(self):
+        self.from_zero_window.deiconify()
+        self.from_zero_window.mainloop()
+
+
 
 
 
@@ -19,4 +61,10 @@ class StartApp():
 if __name__ == "__main__":
     app = StartApp()
     app.root.title("XPCalculator")
+    print(font.families())
     app.root.mainloop()
+
+
+    # print(font.families())  < - - Use this if you want to print the font familes th are stored on your computer
+   
+    
--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # XPCalculator
 
 ### A Tkinter Application that can be used to calculate various actions in the game *Minecraft* and return how long or how much exp is needed to reach certain levels. This will be useful for figuring out:
--How many *Bottle O' Enchantment* bottles are needed to raise you to a certain level
--Average range of time needed at a grinder to raise up to a certain level
--Many other features! ~~As suggested by peers~~
 
-##Enjoy!
+- How many *Bottle O' Enchantment* bottles are needed to raise you to a certain level
+- Average range of time needed at a grinder to raise up to a certain level
+- Many other features! ~~As suggested by peers~~
+
+## Enjoy!
+
+# Current Features
+
+- `Calculate From Level Zero` is a feature that will calculate the total amount of experience needed when a player has a starting level of `0`. Though this feature isn't overally useful, it should still be included as an option because **math**.
+
+
