ec252a3c4785a4350d822cf8e33527da8c6ab77b
star8kid <2022younganthonyn@aaps.k12.mi.us>
2020-06-21 17:19:11 -0400

    Make the feature functional with an entry widget

--- Calculator/main.py
+++ Calculator/main.py
@@ -4,6 +4,8 @@ from tkinter import font
 import time
 
 
+
+
 class StartApp():
     def __init__(self):
 
@@ -29,8 +31,6 @@ class StartApp():
         self.menuQuit.grid( row = 4, column = 1 , pady = self.padVerticalValue)
 
 
-        self.from_zero_window = Toplevel()
-        self.from_zero_window.title("Calculate From Level Zero")
         
 
 
@@ -38,33 +38,72 @@ class StartApp():
 
         # 2. The button configurations
 
-        self.menuButton1.config( text = "Calculate From Level Zero", command = lambda : self.openCalcZero())
 
         self.quitStyle = ttk.Style()
         self.quitStyle.configure("Quit.TButton", foreground = "dark red")
         self.menuQuit.config( text = "Quit" , style = "Quit.TButton" , command = lambda : self.root.quit())
 
-        self.from_zero_window.withdraw()
+     
 
         self.menu_window.pack()
-    # 3. The method configurations
+    # 3. The method definitions/configurations
 
-    def openCalcZero(self):
-        self.from_zero_window.deiconify()
-        self.from_zero_window.mainloop()
+    def openFeatureWindow(self, window_referance):
+        window_referance.deiconify()
+        window_referance.mainloop()
 
 
+
+class FromLevelZero():
+    def __init__(self, feature_title):
+
+        self.feature_title = feature_title
+        self.root = Tk()
+    
+        self.title_frame = ttk.Frame(self.root, padding = "50 10 50 10")
+        self.feature1_font = font.Font( family = "Times New Roman", size = 12 , weight = "bold")
+        self.title_label = ttk.Label(self.title_frame, text = "From Level Zero", font = self.feature1_font, padding = "40 50 40 100")
+        self.title_label.grid( row = 1 , column = 1)
+        self.title_frame.pack()
+
+
+        # For this feature of the program, the software will allow an input of
+        # a single level integer and calculate the amount of experience required to reach that level from level 0
+        self.feature_frame = ttk.Frame(self.root, padding = "10 10 10 10")
+        self.feature_frame.pack()
+
+        self.targetLevel = StringVar()
+        self.totalNeededXPNum = StringVar()
+
+        
+
+
+
+        
 
 
 
 
 if __name__ == "__main__":
+
+    #App initialization
     app = StartApp()
+    feature1 = FromLevelZero("Calculate From Level Zero")
+    feature1.root.withdraw()
+
+    #Window Configurations
     app.root.title("XPCalculator")
-    print(font.families())
+    feature1.root.title(feature1.feature_title)
+
+    app.menuButton1.config( text = feature1.feature_title, command = lambda : app.openFeatureWindow(feature1.root) )
+
+
+
+    #Start of the app
     app.root.mainloop()
 
 
-    # print(font.families())  < - - Use this if you want to print the font familes th are stored on your computer
+    #print(font.families())  
+    # ^^^^^^^ Use this if you want to print the font familes th are stored on your computer
    
     
